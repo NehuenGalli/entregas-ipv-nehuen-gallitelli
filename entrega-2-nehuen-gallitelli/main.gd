680cc490de729a17b9c2fd36c6e206cf58c1979c
Nehuen Gallitelli <nehuengallijans@gmail.com>
2026-09-01 17:20:27 -0300

    Correcion de comportamiento de torretas

--- entrega-2-nehuen-gallitelli/main.gd
+++ entrega-2-nehuen-gallitelli/main.gd
@@ -4,4 +4,4 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Player.set_projectile_container(self)
-	$Turret.set_values($Player, self)
+	get_tree().call_group("turrets", "set_values", $Player, self)
